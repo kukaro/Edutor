a387bc7da875b9af0d7ca8f2ec6c060a02913eba
KTJ <justkukaro@naver.com>
2017-09-16 03:57:50 +0900

    check already in db

--- RESTfulServer/route/PointCut.py
+++ RESTfulServer/route/PointCut.py
@@ -22,19 +22,26 @@ class PointCut(Resource):
         file_header = fileP[0]
         sw = True
         print(fileP[0])
-        if data['term_type'] == '수능':
-            query = [{'term': {
-                'year': data['year'][:4],
-                'subject': data['subject'],
-                'term_type': data['term_type']
-            }, 'point_cut': {}}]
-            for row in fileP:
-                tmp = {}
-                if sw:
-                    sw = False
-                else:
-                    for i in range(len(row)):
-                        tmp[file_header[i]] = row[i]
-                    query[0]['point_cut'][str(row[0])] = tmp
-            print(query)
-            point_cut.insert(query)
+        if point_cut.find_one({'term': {
+            'year': data['year'][:4],
+            'subject': data['subject'],
+            'term_type': data['term_type']
+        }}) is not None:
+            if data['term_type'] == '수능':
+                query = [{'term': {
+                    'year': data['year'][:4],
+                    'subject': data['subject'],
+                    'term_type': data['term_type']
+                }, 'point_cut': {}}]
+                for row in fileP:
+                    tmp = {}
+                    if sw:
+                        sw = False
+                    else:
+                        for i in range(len(row)):
+                            tmp[file_header[i]] = row[i]
+                        query[0]['point_cut'][str(row[0])] = tmp
+                print(query)
+                point_cut.insert(query)
+        return {'success': 'ok'}
+
