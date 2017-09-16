ea0a1a4765617f6f3522110a8b97096bc72a6ba7
limzz <stella_lim@pukyong.ac.kr>
2017-09-16 12:17:13 +0900

    commit message

--- ViewServer/route/left.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, make_response, redirect, request
-from flask_restful import Resource, Api, reqparse
-
-class Left(Resource):
-    def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('view-left-frame.html'), 200, headers)
--- ViewServer/route/term_load.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, make_response, redirect, request
-from flask_restful import Resource, Api, reqparse
-
-class TermAnswer(Resource):
-    def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('term-answer-frame.html'), 200, headers)
